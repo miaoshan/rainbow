cdb9ce443a5235898ce1ed1bcf3144a158281c67
miaoshan <minnieshan@gmail.com>
2019-09-21 22:50:35 +0100

    fetch books

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, { Component } from "react";
-import { Route, Switch, withRouter } from "react-router-dom"; // get access to match,location and history.
-import { BrowserRouter as Router } from "react-router-dom";
+import React, {
+  Component
+} from "react";
+import {
+  Route,
+  Switch,
+  withRouter
+} from "react-router-dom"; // get access to match,location and history.
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
 
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Books from "./components/Books";
 
-
-
-
-// const baseUrl = "https://www.googleapis.com/books/v1/volumes";
+const baseUrl = "https://www.googleapis.com/books/v1/volumes";
 
 
 class App extends Component {
@@ -18,44 +23,99 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      books: []
+      books: [],
+      currentUser: {},
+      currentUsersWishlist: [],
+      currentUsersOrders: [],
+      currentUsersCartBooks: [],
+      sortBy: "All"
     };
   }
+
+
+  fetchBooksFromSever = () => {
+    return fetch(
+      baseUrl +
+      '?q=""&printType=books&orderBy=newest&startIndex=1&maxResults=40&langRestrict=en'
+    ).then(resp => resp.json());
+  };
+
+
   render() {
-    return (
-      <Router>
-        <div className="App" >
-          <NavBar />
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/login" component={(routerProps) => <Login {...routerProps} fetchUsers={this.fetchUsers} setUser={this.setUser} />} />
-            <Route
-              path="/login"
-              exact
-              render={() => (
-                <Login fetchUsers={this.fetchUsers} setUser={this.setUser} />
-              )}
+    return ( <
+      Router >
+      <
+      div className = "App" >
+      <
+      NavBar / >
+      <
+      Switch >
+      <
+      Route path = "/"
+      exact component = {
+        Home
+      }
+      /> <
+      Route path = "/login"
+      component = {
+        (routerProps) => < Login {
+          ...routerProps
+        }
+        fetchUsers = {
+          this.fetchUsers
+        }
+        setUser = {
+          this.setUser
+        }
+        />} / >
+        <
+        Route
+        path = "/login"
+        exact
+        render = {
+          () => ( <
+            Login fetchUsers = {
+              this.fetchUsers
+            }
+            setUser = {
+              this.setUser
+            }
             />
+          )
+        }
+        />
 
-            <Route
-              path="/books"
-              exact
-              render={props => (
-                <Books
-                  {...props}
-                  books={this.getSortedBooks()}
-                  getFilteredBooksFromServer={this.getFilteredBooksFromServer}
-                  resetBooks={this.resetBooks}
-                  setSortBy={this.setSortBy}
-                />
-              )}
+        <
+        Route
+        path = "/books"
+        exact
+        render = {
+          props => ( <
+            Books {
+              ...props
+            }
+            // books = {
+            //   this.getSortedBooks()
+            // }
+            getFilteredBooksFromServer = {
+              this.getFilteredBooksFromServer
+            }
+            resetBooks = {
+              this.resetBooks
+            }
+            setSortBy = {
+              this.setSortBy
+            }
             />
-          </Switch>
-        </div>
-      </Router>
+          )
+        }
+        /> < /
+        Switch > <
+        /div> < /
+        Router >
 
-    );
+      );
+    }
   }
-}
 
-export default App;
+  export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
-import React, { Component } from "react";
+import React, {
+    Component
+} from "react";
 
 class Home extends Component {
     render() {
-        return <div>Home Page</div>;
+        return <div > Home Page < /div>;
     }
 }
 
